1ec1ec4d7d806937565a825429a9e0897f07e997
Jonathan A. Kimball <jonathan.kimball64@gmail.com>
2019-10-18 09:00:04 -0700

    jsdoc setup

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # l04-data-modeling
+
+## Project Name
+
+### Author: Student/Group Name
+
+### Links and Resources
+* [submission PR](https://github.com/401-advanced-javascript-kimball/l04-classes/pull/1)
+* [travis](https://travis-ci.com/401-advanced-javascript-kimball/l04-classes)
+* [front-end](https://jk-401js-lab04.herokuapp.com) (when applicable)
+
+#### Documentation
+<!-- * [jsdoc](https://jk-401js-lab02.herokuapp.com/docs) -->
+<!-- 
+### Modules
+#### `modulename.js`
+##### Exported Values and Methods
+
+###### `foo(thing) -> string`
+Usage Notes or examples
+
+###### `bar(array) -> array`
+Usage Notes or examples
+
+### Setup
+#### `.env` requirements
+* `PORT` - Port Number
+* `MONGODB_URI` - URL to the running mongo instance/db
+
+#### Running the app
+* `npm start`
+* Endpoint: `/foo/bar/`
+  * Returns a JSON object with abc in it.
+* Endpoint: `/bing/zing/`
+  * Returns a JSON object with xyz in it.
+  
+#### Tests
+* How do you run tests?
+* What assertions were made?
+* What assertions need to be / should be made?
+
+#### UML
+Link to an image of the UML for your application and response to events -->
---
+++ models/__tests__/categories.test.js
@@ -0,0 +1,47 @@
+const Categories = require('../categories/categories.js');
+
+describe('Categories Model', () => {
+
+  let categories;
+
+  beforeEach(() => {
+    categories = new Categories();
+  })
+
+  // How might we repeat this to check on types?
+  it('sanitize() returns undefined with missing requirements', () => {
+    const schema = categories.schema;
+    var testRecord = {};
+    for (var field in schema) {
+      if (schema[field].required) {
+        testRecord[field] = null;
+      }
+    }
+    expect(categories.sanitize(testRecord)).toBeUndefined();
+  });
+
+  it('can post() a new category', () => {
+    let obj = { name: 'Test Category' };
+    return categories.create(obj)
+      .then(record => {
+        Object.keys(obj).forEach(key => {
+          expect(record[key]).toEqual(obj[key]);
+        });
+      })
+      .catch(e => console.error('ERR', e));
+  });
+
+  it('can get() a category', () => {
+    let obj = { name: 'Test Category' };
+    return categories.create(obj)
+      .then(record => {
+        return categories.get(record._id)
+          .then(category => {
+            Object.keys(obj).forEach(key => {
+              expect(category[0][key]).toEqual(obj[key]);
+            });
+          });
+      });
+  });
+
+});
